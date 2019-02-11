3709c45068d1f292d54b7f69f218896ad2221420
GregoryLevinson <gregory.levinson@loop.colum.edu>
2019-02-10 23:48:16 -0600

    Add files via upload

---
+++ AStar_/Agent/GameMap.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+
+namespace AStar
+{
+    public class GameMap
+    {
+        static List<Points> BarrierPoints = new List<Points>()
+        {
+            new Points(1,2,true),
+            new Points(2,5,true),
+            new Points(2,6,true),
+            new Points(2,7,true),
+            new Points(3,1,true),
+            new Points(3,2,true),
+            new Points(3,3,true),
+            new Points(4,7,true),
+            new Points(4,8,true),
+            new Points(4,9,true),
+            new Points(5,0,true),
+            new Points(5,1,true),
+            new Points(5,2,true),
+            new Points(6,5,true),
+            new Points(8,1,true),
+            new Points(8,2,true),
+            new Points(8,3,true),
+            new Points(8,4,true),
+            new Points(8,7,true),
+            new Points(8,8,true),
+            new Points(8,9,true),
+        };
+        /*//0    1   2     3     4    5    6    7   8    9
+        {"w", "w", 'w', 'w', 'w', 'g', 'w', 'w', 'W', 'w'},//0
+        {'w', 'w', 'w', 'g', 'w', 'g', 'w', 'w', 'g', 'w'},//1
+        {'w', 'g', 'w', 'g', 'w', 'g', 'w', 'w', 'g', 'w'},//2
+        {'w', 'w', 'w', 'g', 'w', 'w', 'w', 'w', 'g', 'w'},//3
+        {'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'g', 'w'},//4
+        {'w', 'w', 'g', 'w', 'w', 'w', 'g', 'w', 'w', 'w'},//5
+        {'w', 'w', 'g', 'w', 'g', 'w', 'g', 'w', 'w', 'w'},//6
+        {'w', 'w', 'g', 'w', 'g', 'w', 'w', 'w', 'g', 'w'},//7
+        {'w', 'w', 'w', 'w', 'g', 'w', 'w', 'w', 'g', 'w'},//8
+        {'w', 'w', 'w', 'w', 'g', 'w', 'w', 'w', 'g', 'w'} //9
+        */
+        public static Points[,] GameMapPoints = new Points[10, 10];
+
+        public static void GenerateMap()
+        {
+            foreach (var point in BarrierPoints)
+            {
+                GameMapPoints[point.x, point.y] = point;
+            }
+            for (int y = 0; y < 10; y++)
+            {
+                for (int x = 0; x < 10; x++)
+                {
+
+                    if (GameMapPoints[x, y] == null)
+                    {
+                        GameMapPoints[x, y] = new Points(x, y, false);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ AStar_/Agent/Points.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AStar
+{
+    public class Points
+    {
+        public int x  { get; set; }
+        public int y { get; set; }
+        public int FCost;
+        public int HCost;
+        public int GCost;
+        public bool isBarrier { get; private set; }
+        public bool isClosed { get; private set; }
+        public Points(int x, int y, bool isBarrier)
+        {
+            this.x = x;
+            this.y = y;
+            this.isBarrier = isBarrier;
+        }
+    }
+}
